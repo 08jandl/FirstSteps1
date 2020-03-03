fcea86ac72cbd400c99c0b3937cbba2bfa95ebe8
Silke Jandl <silke8421@gmx.at>
2020-03-03 12:01:33 +0100

    Initial commit

---
+++ src/Hello.java
@@ -0,0 +1,10 @@
+public class Hello {
+    public static void main (String[] args) {
+        System.out.print("Hello world");
+        System.out.println();
+        int i = 2;
+        double d = 1.5;
+        int x = i * ((int)d);
+        System.out.println("Campus 0"+x);
+    }
+}
